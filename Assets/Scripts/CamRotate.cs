e7f5137e03112ca852429644a738490b7c68e7d1
HooJungKim <sdhh1735@gmail.com>
2023-03-19 21:28:25 +0900

    Update CamRotate.cs

--- Assets/Scripts/CamRotate.cs
+++ Assets/Scripts/CamRotate.cs
@@ -4,35 +4,37 @@ using UnityEngine;
 
 public class CamRotate : MonoBehaviour
 {
-    Vector3 angle;
+    Vector3 cameraAngle;
 
-    public float sensitivity = 200.0f;
+    private float angleXMin = -80.0f; // 카메라 x축 회전 범위 (최소)
+    private float angleXMax = 50.0f; // 카메라 x축 회전 범위 (최대)
+
+    [SerializeField] float rotateSpeed = 200.0f;
+    //[SerializeField] float rotateSpeedY = 200.0f;
 
     void Start()
     {
         // 시작할 때 현재 카메라의 각도를 적용
-        angle.y = -Camera.main.transform.eulerAngles.x;
-        angle.x = Camera.main.transform.eulerAngles.y;
-        angle.z = Camera.main.transform.eulerAngles.z;
+        cameraAngle.x = -Camera.main.transform.eulerAngles.x;
+        cameraAngle.y = Camera.main.transform.eulerAngles.y;
+        cameraAngle.z = Camera.main.transform.eulerAngles.z;
 
     }
 
-    
+
     void Update()
     {
-        // 마우스 입력에 따라 카메라를 회전시키고 싶다.
-        // 1. 사용자의 마우스 입력을 얻어와야 한다.
-        // 마우스의 좌우 입력을 받는다.
-        float x = Input.GetAxis("Mouse X");
-        float y = Input.GetAxis("Mouse Y");
+        // 마우스의 좌우 입력을 가져온다.
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
 
-        // 2. 방향이 필요하다.
         // 이동 공식에 대입해 각 속성별로 회전 값을 누적시킨다.
-        angle.x += x * sensitivity * Time.deltaTime;
-        angle.y += y * sensitivity * Time.deltaTime;
+        cameraAngle.x -= mouseY * rotateSpeed * Time.deltaTime;
+        cameraAngle.y += mouseX * rotateSpeed * Time.deltaTime;
 
-        // 3. 회전시키고 싶다.
+        cameraAngle.x = Mathf.Clamp(cameraAngle.x, angleXMin, angleXMax);
+
         // 카메라의 회전 값에 새로 만들어진 회전 값을 할당한다.
-        transform.eulerAngles = new Vector3(-angle.y, angle.x, transform.eulerAngles.z);
+        transform.eulerAngles = new Vector3(cameraAngle.x, cameraAngle.y, transform.eulerAngles.z);
     }
 }
