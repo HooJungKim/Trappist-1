5cc26a68808338f9f6808173401db9b42079ecab
HooJungKim <sdhh1735@gmail.com>
2023-03-30 17:32:30 +0900

    Update AudioManager.cs

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -6,6 +6,7 @@ public class AudioManager : MonoBehaviour
 {
     AudioSource _audioSource;
     public static float[] _samples = new float[512];
+    public static float[] _freqBand = new float[8];
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +18,26 @@ public class AudioManager : MonoBehaviour
     void Update()
     {
         GetSpectrumAudioSource();
+        MakeFrequencyBands();
     }
 
     void GetSpectrumAudioSource()
     {
         _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+    
+    void MakeFrequencyBands()
+    {
+        /*
+         * 22050 / 512 = 43hertz per sample
+         * 
+         * 20 - 60 hertz
+         * 60 - 250 hertz
+         * 250 - 500 hertz
+         * 500 - 2000 hertz
+         * 2000 - 4000 hertz
+         * 4000 - 6000 hertz
+         * 6000 - 20000 hertz
+         */
     }
 }
