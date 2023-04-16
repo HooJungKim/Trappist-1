2341d4c83f06448740d32d8b1a78e98915491da3
tmdgusql <tmdgusql@naver.com>
2023-04-16 12:48:14 +0900

    Revert "Merge branch 'main' of https://github.com/HooJungKim/Trappist-1"
    This reverts commit c91684e0a6adce1283ed17a453d5c49ad37c6b77, reversing
    changes made to 14ef382884be19cc9758af0b8e557eb984610844.

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
-    // 음원 주파수 대역 별 추출 인자 -> 총 8개의 대역으로 구분
     AudioSource _audioSource;
     public static float[] _samples = new float[512];
-    public static float[] _freqBand = new float[8];
-
-    // 시작하고나서의 시간
-    public static int timeCheck = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,58 +17,10 @@ public class AudioManager : MonoBehaviour
     void Update()
     {
         GetSpectrumAudioSource();
-        MakeFrequencyBands();
     }
 
     void GetSpectrumAudioSource()
     {
         _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
-    }
-    
-    void MakeFrequencyBands()
-    {
-        /*
-         * 22050 / 512 = 43hertz per sample
-         * 
-         * 20 - 60 hertz
-         * 60 - 250 hertz
-         * 250 - 500 hertz
-         * 500 - 2000 hertz
-         * 2000 - 4000 hertz
-         * 4000 - 6000 hertz
-         * 6000 - 20000 hertz
-         * 
-         * 0 - 2 = 86hertz
-         * 1 - 4 = 172 hertz - 87-258
-         * 2 - 8 = 344 hertz - 259-602
-         * 3 - 16 = 688 hertz - 603-1290
-         * 4 - 32 = 1376 hertz - 1291-2666
-         * 5 - 64 = 2752 hertz - 2667-5418
-         * 6 - 128 = 5504 hertz - 5419-10922
-         * 7 - 256 = 11008 hertz - 10923-21930
-         * 510
-         */
-
-        int count = 0;
-
-        for (int i = 0; i < 8; i++)
-        {
-            float average = 0;
-            int sampleCount = (int)Mathf.Pow(2, i) * 2;
-
-            if(i == 7)
-            {
-                sampleCount += 2;
-            }
-            for (int j = 0; j < sampleCount; j++)
-            {
-                average += _samples[count] * (count + 1);
-                count++;
-            }
-
-            average /= count;
-            _freqBand[i] = average * 10;
-        }
-
     }
 }
--- Assets/Scripts/Note/InstantiateNote.cs
+++ Assets/Scripts/Note/InstantiateNote.cs
@@ -9,12 +9,9 @@ public class InstantiateNote : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 512; i++){
-            GameObject _instanceSamepleCube = (GameObject)Instantiate(_sampleCubePrefab);
-            _instanceSamepleCube.transform.position = this.transform.position;
-            _instanceSamepleCube.transform.parent = this.transform;
-            _instanceSamepleCube.name = "SampleCube" + i;
-        }
+        //for (int i = 0;, i < 512; i++){
+        //    GameObject _instanceSamepleCube = (GameObject)Instantiate(_sampleCubePrefab);
+        //}
     }
 
     // Update is called once per frame
