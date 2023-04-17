a9e6d05598bbfca409c3b2fbe4ed0f8121030775
tmdgusql <tmdgusql@naver.com>
2023-04-17 15:23:35 +0900

    UI automation
    1st

---
+++ Assets/Scripts/UI/UI_Button.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_Button : UI_Base
+{
+    Dictionary<System.Type, UnityEngine.Object[]> _objects = new Dictionary<System.Type, UnityEngine.Object[]>();
+    enum Buttons
+    {
+        PointButton
+    }
+
+    enum Texts
+    {
+        PointText,
+        ScoreText
+    }
+
+    enum GameObjects
+    {
+        TestObject,
+    }
+
+    enum Images
+    {
+        ItemIcon,
+    }
+
+    private void Start()
+    {
+        // reflection 사용하여 자동화
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
+        Bind<Image>(typeof(Images));
+
+        Get<Button>((int)Buttons.PointButton).gameObject.AddUIEvent(OnButtonClicked);
+
+        GameObject go = GetImage((int)Images.ItemIcon).gameObject;
+        AddUIEvent(go, (PointerEventData data) => { go.transform.position = data.position; }, Define.UIEvent.Drag);
+    }
+
+    int _score = 0;
+    public void OnButtonClicked(PointerEventData data)
+    {
+        Debug.Log("ButtonClikced");
+
+        Get<Text>((int)Texts.PointText).text = $"Score : {_score}";
+
+    }
+
+}
